dca739bf097d119e24149a57eb96dde92b1fc61b
[Anup  Magar] <am6956275@gmail.com>
2025-02-23 21:23:28 +0545

    Sorting

--- SortingAlgorithm.py
+++ SortingAlgorithm.py
@@ -17,3 +17,20 @@ def draw_data(data, color_array):
     ax.bar(range(len(data)), data, color=color_array)
     canvas.draw()
     root.update_idletasks()  # Smooth UI update
+
+def bubble_sort():
+    """Perform Bubble Sort and visualize it."""
+    global data
+    n = len(data)
+    if n == 0:
+        messagebox.showerror("Error", "No data to sort!")
+        return
+
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if data[j] > data[j + 1]:
+                data[j], data[j + 1] = data[j + 1], data[j]
+                draw_data(data, ['red' if x == j or x == j+1 else 'blue' for x in range(n)])
+                time.sleep(0.05)
+
+    draw_data(data, ['green' for _ in range(n)])
